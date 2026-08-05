1acb4f947541337b1e21314b8b4e0a2c79b5001b
silong <1521424150@qq.com>
2026-08-05 11:15:43 +0800

    chore：initialize spring boot project

---
+++ src/main/java/com/silong/usermatch/UserMatchApplication.java
@@ -0,0 +1,13 @@
+package com.silong.usermatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silong/usermatch/UserMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silong.usermatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
